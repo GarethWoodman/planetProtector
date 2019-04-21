c78004d374aa6a3f2e08f1d054b27ad1c5335c7d
Gareth Woodman <gareth.woodman92@gmail.com>
2019-04-21 01:23:17 +0100

    Removed redundant function

--- planetProtector/GameScene.swift
+++ planetProtector/GameScene.swift
@@ -9,16 +9,6 @@
 import SpriteKit
 import GameplayKit
 
-extension Array where Element: Equatable {
-    
-    // Remove first collection element that is equal to the given `object`:
-    mutating func remove(object: Element) {
-        guard let index = firstIndex(of: object) else {return}
-        remove(at: index)
-    }
-    
-}
-
 class GameScene: SKScene {
     
     var player = SKSpriteNode()
